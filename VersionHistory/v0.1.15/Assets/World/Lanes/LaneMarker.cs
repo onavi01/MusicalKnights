ca09cbc535610f1f9ed5cf6bd9063eaccba9fe73
onavi01 <77210778+onavi01@users.noreply.github.com>
2021-05-04 18:36:55 -0400

    Patch v0.1.15

---
+++ VersionHistory/v0.1.15/Assets/World/Lanes/LaneMarker.cs
@@ -0,0 +1,9 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LaneMarker : MonoBehaviour {
+    public GameObject occupyingEntity;
+    public bool isOccupied = false;
+    public int markerID = 0;
+}
