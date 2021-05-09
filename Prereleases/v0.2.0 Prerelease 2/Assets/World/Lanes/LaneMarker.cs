88e89ce2e75c6e0724332259aa940ca6efc95670
onavi01 <77210778+onavi01@users.noreply.github.com>
2021-05-09 00:51:26 -0400

    Push backup - v0.2.0 Prerelease 2
    + Create 'UserSettings' folder
    + Update .gitignore

---
+++ Prereleases/v0.2.0 Prerelease 2/Assets/World/Lanes/LaneMarker.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LaneMarker : MonoBehaviour {
+    private SpriteRenderer rend;
+    public GameObject occupyingEntity;
+    public bool isOccupied = false, friendlyOccupied = false;
+    public int markerID = 0;
+
+    void Start() {
+        rend = GetComponent<SpriteRenderer>();
+    }
+
+    void Update() {
+        /*Temp - colour markers based on occupancy*/
+        if ( isOccupied ) {
+            if ( friendlyOccupied ) {
+                rend.color = new Color(0f, 0f, 1f, 0.35f);
+            } else {
+                rend.color = new Color(1f, 0f, 0f, 0.35f);
+            }
+        } else {
+            rend.color = new Color(1f, 1f, 1f, 0.35f);
+        }
+    }
+}
